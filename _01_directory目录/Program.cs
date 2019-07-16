2a57eb5258217e398e2a0e357bbae8bdaa358e30
ocxz <2252779530@qq.com>
2019-07-16 17:15:38 +0800

    添加项目文件。

---
+++ _01_directory目录/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_directory目录
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
