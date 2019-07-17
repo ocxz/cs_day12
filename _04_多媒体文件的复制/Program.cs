0a9728c331b826d2d5f371e013bb7675f0abe52e
ocxz <2252779530@qq.com>
2019-07-17 10:42:55 +0800

    第十二天学习完成，主要内容是文件、流的操作

---
+++ _04_多媒体文件的复制/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+
+namespace _04_多媒体文件的复制
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 利用FileStream实现多媒体文件复制
+
+            string srcFilePath = @"F:\c#代码\第十一天\第十一天解决方案\_17_File类的写操作\bin\Debug\netcoreapp2.1\new2.txt";
+            string destFilePath = @"new3.txt";
+
+            FileCopy(srcFilePath, destFilePath);
+            Console.WriteLine("文件{0}复制完成", Path.GetFileName(srcFilePath));
+            Console.ReadKey();
+
+            #endregion
+
+            #region MyRegion
+
+            //string srcFilePath = @"F:\c#代码\第十一天\第十一天解决方案\_17_File类的写操作\bin\Debug\netcoreapp2.1\new2.txt";
+            //string destFilePath = @"new3.txt";
+
+            //FileCopyTwo(srcFilePath, destFilePath);
+            //Console.WriteLine("{0}复制完成", Path.GetFileName(srcFilePath));
+            //Console.ReadKey();
+
+            #endregion
+        }
+
+        /// <summary>
+        /// 多媒体文件的复制
+        /// </summary>
+        /// <param name="sorucePath">源文件路径</param>
+        /// <param name="destPath">目标文件路径</param>
+        public static void FileCopy(string srcFilePath, string destFilePath)
+        {
+            using (FileStream fsRead = new FileStream(srcFilePath, FileMode.Open, FileAccess.Read))
+            {
+                using (FileStream fsWrite = new FileStream(destFilePath, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    // 创建缓冲区
+                    byte[] buffer = new byte[1024 * 1024];   // 1M的缓冲区
+
+                    while (true)
+                    {
+                        // 读入缓冲区,返回读到的字节数 当读取完成后，返回r=0
+                        int r = fsRead.Read(buffer, 0, buffer.Length);
+
+                        if (r == 0)   // 读入完成，直接退出当前方法
+                        {
+                            return;
+                        }
+                        else
+                        {
+                            fsWrite.Write(buffer, 0, r);   // r表示实际读到的字节数，写入时不能多写
+                        }
+                    }
+                    
+                }
+            }
+        }
+
+        public static void FileCopyTwo(string srcFilePath,string destFilePath)
+        {
+            byte[] buffer = File.ReadAllBytes(srcFilePath);
+
+            File.WriteAllBytes(destFilePath, buffer);
+        }
+    }
+
+}
---
+++ _05_Stream/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace _05_Stream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /**
+             *   使用StreamReader 来读取数据
+             *   注意：FileStream读的是字节、Stream文件流读的是字符
+             *   第一种方法：利用FileStream来读取文件，然后利用StreamReader来读取流
+             *   第二种方法：直接用StreamReader来读取文件
+             *   
+             *   使用StreamWrite 来写数据
+             * 
+             */
+
+
+            #region 一、读  第一种方法：利用FileStream来读取文件，然后利用StreamReader来读取流
+            ////使用FileStream读取文件
+            //string srcFilePath = @"F:\c#代码\第十一天\第十一天解决方案\_17_File类的写操作\bin\Debug\netcoreapp2.1\new22.txt";
+            //using (FileStream fsRead = new FileStream(srcFilePath, FileMode.Open, FileAccess.Read))
+            //{
+            //    using (StreamReader sr = new StreamReader(fsRead, Encoding.Default))    // 使用Stream读取流
+            //    {
+            //        while (!sr.EndOfStream)   // 流还没结束 就循环读取
+            //        {
+            //            Console.WriteLine(sr.ReadLine());
+            //        }
+            //    }
+            //}
+            //Console.ReadKey();
+            #endregion
+
+            #region 一、读  第二种方法 直接用StreamReader来读取文件
+            ////第二种方法 直接用StreamReader来读取文件
+
+            //string srcFilePath = @"F:\c#代码\第十一天\第十一天解决方案\_17_File类的写操作\bin\Debug\netcoreapp2.1\new22.txt";
+
+            //using (StreamReader sr = new StreamReader(srcFilePath, Encoding.Default))
+            //{
+            //    while (!sr.EndOfStream)
+            //    {
+            //        Console.WriteLine(sr.ReadLine());
+            //    }
+            //}
+            //Console.ReadKey();
+
+            #endregion
+
+            #region 二、写  第一种方法：利用fsWriter写入到文件 利用StreamWriter写入到fsWriter流中 部分覆盖
+
+            //string filePath = @"text.txt";
+            //using (FileStream fsWriter = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write))
+            //{
+            //    using (StreamWriter sw = new StreamWriter(fsWriter, Encoding.Default))
+            //    {
+            //        sw.Write("是呀，我们一起出去旅行吧！");
+            //    }
+            //}
+
+            //Console.WriteLine("写入{0}文件成功", Path.GetFileName(filePath));
+            //Console.ReadKey();
+
+            #endregion
+
+            #region  二、写  第二种方法：直接利用StreamWriter写入到文件中 全覆盖
+
+            //string filePath = @"text.txt";
+            //using (StreamWriter sw = new StreamWriter(filePath, true, Encoding.Default))
+            //{
+            //    sw.WriteLine("啦啦啦，我是卖报的小当家");
+            //}
+
+            //Console.WriteLine("写入{0}文件成功", Path.GetFileName(filePath));
+            //Console.ReadKey();
+
+            //#endregion
+        }
+    }
+}
